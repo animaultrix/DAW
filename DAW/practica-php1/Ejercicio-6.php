8643f71ff687432636d7f2661f47fb48ea7fbbed
Vindex <vindex.pg@gmail.com>
2021-05-15 11:29:19 +0100

    Ejercicio-6 con str_split()

--- DAW/practica-php1/Ejercicio-6.php
+++ DAW/practica-php1/Ejercicio-6.php
@@ -8,32 +8,64 @@ debemos explicar cómo lo haríamos. -->
     <meta charset="UTF-8">
     <title>Ejercicio-6</title>
     <style>
-            .centrar{
+        .caja{
+            position: absolute;
+            top:50%;
+            left: 50%;
+            height: 400px;
+            width: 400px;
+            margin-top: -300px;
+            margin-left: -215px;
+            padding: 30px;               
+            background-color: gainsboro;
+            border-radius: 9px;
             text-align: center;
-            margin-top:100px;
-            }
-    </style>    
+        }  
+    </style>
 </head>
 <body>
-    <div>     
+    <div class="centrar caja">     
         <form action="" method="post" class="centrar">
-            <h2>Introduce un número mayor a 0</h2>
-            <input type="text" id="entrada" name="entrada">
+            <h2 style="padding-top: 33px;">Introduce un número mayor a 0</h2>
+            <input oninput="if(this.value.length > this.maxLength)" type="text" maxlength="17" id="entrada" name="entrada">
             <input type="submit" value="convertir a texto" id="enviar" name="enviar">
         </form>
-        
-    </div>
-    <?php
-        if (isset($_POST["enviar"])) {
+        <?php
 
-            $numero = str_split($_POST["entrada"]); 
-            echo "<h3 class=\"centrar\">";
-            foreach ($numero as $key) {
-                global $cadena;
-                $cadena .= $key." ";
-            }
-            echo "$cadena </h3>";            
-        }  
-    ?>
+            global $cadena;
+            global $noNum;
+            if (isset($_POST["enviar"])) {
+
+                $numeros = str_split($_POST["entrada"]);
+                foreach ($numeros as $key) {
+                    
+                    switch ($key) {
+
+                        case '1': $cadena .= "uno "; break;
+                        case '2': $cadena .= "dos "; break;
+                        case '3': $cadena .= "tres "; break;
+                        case '4': $cadena .= "cuatro "; break;
+                        case '5': $cadena .= "cinco "; break;
+                        case '6': $cadena .= "seis "; break;
+                        case '7': $cadena .= "siete "; break;
+                        case '8': $cadena .= "ocho "; break;
+                        case '9': $cadena .= "nueve "; break;
+                        case ' ': $cadena .= ""; break;
+                        case '': $cadena .= ""; break;
+                        default: $noNum .= "'".$key."' ";
+                    }                
+                }
+                echo '';
+                echo "<h3>".$_POST["entrada"]."</h3>";
+                echo $cadena != "" ? "<hr style='width: 200px;'>" : "";            
+                echo "<h3>$cadena</h3>";
+                if ($noNum!=null) {
+                    echo "<h3><span style='color: red;'>No son numeros: $noNum.</span></h3>";
+                }else if ($cadena==null){
+                    echo "<h3><span style='color: red;'>Vacio.</span></h3>";
+                }                 
+            } 
+        ?>
+    </div>
 </body>
 </html>
